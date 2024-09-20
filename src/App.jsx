de1f53ed2a4d3af24540d5a0e322c86a63a6e4f4
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-20 14:17:06 +0500

    Some Changes !!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store"; // Adjust the path if necessary
 import Dashboard from "./components/dashboard/Dashboard";
+import Login from "./components/login/Login";
+import Singup from "./components/signup/Signup";
+import PrivateRoute from "./components/PrivateRoute"; // Adjust the path if necessary
+import Signup from "./components/signup/Signup";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<PrivateRoute element={<Dashboard />} />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
