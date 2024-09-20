da9e9dbd6fb24d61f323fae1eb15294bc51b77df
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-20 14:18:21 +0500

    firebase config added

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+const firebaseConfig = {
+  apiKey: "AIzaSyCJO9u-HG5gCwa0lMC5rRj8FJAWczn1dfM",
+  authDomain: "expense-tracker-2ae8c.firebaseapp.com",
+  projectId: "expense-tracker-2ae8c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const db = getDatabase();
+export { auth, db };
