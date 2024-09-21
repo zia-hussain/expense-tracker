79908b611f1ab05cd1b1ffb89f9fbe917f2dce4d
zia-hussain <syedziashahgillani76@gmail.com>
2024-09-22 02:07:52 +0500

    Complete

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import expensesReducer from "./features/expenseSlice";
 import authReducer from "./features/authSlice"; // Import the auth slice reducer
+import { thunk } from "redux-thunk"; // Import thunk
 
 const store = configureStore({
   reducer: {
     expenses: expensesReducer,
     auth: authReducer, // Add auth reducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // Add thunk middleware
 });
 
 export default store;
